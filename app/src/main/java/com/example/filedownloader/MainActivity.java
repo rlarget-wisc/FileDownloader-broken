2415a049ca4e537b783c52f1044c96abdc11efae
Riley Larget <larget@wisc.edu>
2021-10-04 16:11:36 -0500

    M1-6 -> errors as expected

--- app/src/main/java/com/example/filedownloader/MainActivity.java
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -5,18 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+    }
+
+    class ExampleRunnable implements Runnable {
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
     }
 
     public void mockFileDownloader() {
+
+        startButton.setText("DOWNLOADING...");
+
         for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress+=10) {
-            Log.d("TAG","Download Progress: "+downloadProgress+"%");
+            Log.d(TAG,"Download Progress: "+downloadProgress+"%");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -26,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startDownload(View view) {
-        mockFileDownloader();
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
     }
 }

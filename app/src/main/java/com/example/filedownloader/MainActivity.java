a14a5827a8c357cb6db56be29c7676872c4d7709
Riley Larget <larget@wisc.edu>
2021-10-04 16:02:26 -0500

    Only main thread

--- app/src/main/java/com/example/filedownloader/MainActivity.java
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.filedownloader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mockFileDownloader() {
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress+=10) {
+            Log.d("TAG","Download Progress: "+downloadProgress+"%");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void startDownload(View view) {
+        mockFileDownloader();
     }
 }
